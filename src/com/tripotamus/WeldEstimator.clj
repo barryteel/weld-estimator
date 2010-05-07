ed359029a882217184ce45b125c513a69fb085a8
Barry Teel <barry.teel@gmail.com>
2010-05-07 18:29:10 -0400

    adding JTabbedPane to UI

--- src/com/tripotamus/WeldEstimator.clj
+++ src/com/tripotamus/WeldEstimator.clj
@@ -1,14 +1,31 @@
 (ns com.tripotamus.WeldEstimator
   (:gen-class)
-  (:import (java.awt.event WindowAdapter)
-           (javax.swing JFrame UIManager)))
+  (:import (java.awt.event KeyEvent WindowAdapter)
+           (javax.swing JFrame JLabel JPanel JTabbedPane SwingConstants
+                        UIManager)))
 
 (defn save-before-exit []
 	(println "function 'save-before-exit' called"))
 
+;; build 'Define New Weld' panel ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def define-weld-panel (doto (JPanel.)
+  (.add (JLabel. "define-weld-panel" SwingConstants/CENTER))))
+
+;; build 'Current Welds' panel ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def current-welds-panel (doto (JPanel.)
+  (.add (JLabel. "current-welds-panel" SwingConstants/CENTER))))
+
+;; build tabbed pane ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def tabbed-pane (doto (JTabbedPane.)
+  (.addTab "Define New Weld" define-weld-panel)
+  (.setMnemonicAt 0 KeyEvent/VK_1)
+  (.addTab "Current Welds" current-welds-panel)
+  (.setMnemonicAt 1 KeyEvent/VK_2)))
+
 (defn init-GUI []
   (let [frame (JFrame. "Weld Estimator")]
     (doto frame
+      (.setContentPane tabbed-pane)
       (.addWindowListener
         (proxy [WindowAdapter] []
           (windowClosing [event]
